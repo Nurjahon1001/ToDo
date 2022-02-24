58c195bb1c36b90977a71a21eacf957ed697c525
Nurjahon1001 <nurjahonalisherova@gmail.com>
2022-02-24 13:32:35 +0300

    saved to local storage

--- main.js
+++ main.js
@@ -1,208 +1,220 @@
-let ToDo = [
-    { id: 1, title: "Create a button" },
-    { id: 2, title: "Create a edit button" },
-    { id: 3, title: "Create a delete button" },
-    { id: 4, title: "Create a add button" }
-]
+let ToDo = JSON.parse(localStorage.getItem("ToDo")) || [
+  { id: 1, title: "Create a button" },
+  { id: 2, title: "Create a edit button" },
+  { id: 3, title: "Create a delete button" },
+  { id: 4, title: "Create a add button" },
+];
 
-let ndCardList = [];
-let rdCardList = [];
+ 
+let doingCardList = JSON.parse(localStorage.getItem("doingCardList")) || [];
+let doneCardList = JSON.parse(localStorage.getItem("doneCardList")) || [];
 
-let isEdit = false;
-let whichEl = null;
+let isEditMode = false;
+let selectedElement = null;
 
 // constants
-const form = document.querySelector(".form"),
-    form2 = document.querySelector(".form2"),
-    form3 = document.querySelector(".form3"),
-    input = document.querySelector(".input"),
-    input2 = document.querySelector(".input2"),
-    input3 = document.querySelector(".input3"),
-    todos_ul = document.querySelector(".stCard"),
-    ndCard = document.querySelector(".ndCard"),
-    rdCard = document.querySelector(".rdCard");
+const toDoCardForm = document.querySelector(".toDoCardForm"),
+  doingCardForm = document.querySelector(".doingCardForm"),
+  doneCardForm = document.querySelector(".doneCardForm"),
+  toDoCardInput = document.querySelector(".toDoCardInput"),
+  doingCardInput = document.querySelector(".doingCardInput"),
+  doneCardInput = document.querySelector(".doneCardInput"),
+  todoList = document.querySelector(".todoList"),
+  doingList = document.querySelector(".doingList"),
+  doneList = document.querySelector(".doneList");
 
-const WriteTodos = () => {
-    todos_ul.innerHTML = "";
-    ToDo.map((todo, index) => {
-        todos_ul.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
-        <span>${index+1}. ${todo.title}</span>
+const createTodo = () => {
+  todoList.innerHTML = "";
+  ToDo.map((todo, index) => {
+    todoList.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
+        <span>${index + 1}. ${todo.title}</span>
         <span>
             <i onclick="EditToDo(${todo.id})" class="bi bi-pencil-square"></i>
             <i onclick="DeleteToDo(${todo.id})" class="bi bi-trash"></i>
         </span>
-    </li>`
-    })
-}
-const WriteTodos2 = () => {
-    ndCard.innerHTML = "";
-    ndCardList.map((todo2, index2) => {
-        ndCard.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
-        <span>${index2+1}. ${todo2.title}</span>
+    </li>`;
+  });
+
+
+};
+const createDoingList = () => {
+  doingList.innerHTML = "";
+  doingCardList.map((doingli, doingListIndex) => {
+    doingList.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
+        <span>${doingListIndex + 1}. ${doingli.title}</span>
         <span>
-            <i onclick="EditToDo2(${todo2.id})" class="bi bi-pencil-square"></i>
-            <i onclick="DeleteToDo2(${todo2.id})" class="bi bi-trash"></i>
+            <i onclick="editDoingList(${doingli.id})" class="bi bi-pencil-square"></i>
+            <i onclick="deleteDoingList(${doingli.id})" class="bi bi-trash"></i>
         </span>
-    </li>`
-    })
-}
-const WriteTodos3 = () => {
-    rdCard.innerHTML = "";
-    rdCardList.map((todo3, index3) => {
-        rdCard.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
-        <span>${index3+1}. ${todo3.title}</span>
+    </li>`;
+  });
+};
+const createDoneList = () => {
+    doneList.innerHTML = "";
+  doneCardList.map((doneLi, doneListIndex) => {
+    doneList.innerHTML += `<li id="drag1" ondragstart ="drag(event)" draggable="true">
+        <span>${doneListIndex + 1}. ${doneLi.title}</span>
         <span>
-            <i onclick="EditToDo3(${todo3.id})" class="bi bi-pencil-square"></i>
-            <i onclick="DeleteToDo3(${todo3.id})" class="bi bi-trash"></i>
+            <i onclick="editDoneList(${doneLi.id})" class="bi bi-pencil-square"></i>
+            <i onclick="deleteDoneList(${doneLi.id})" class="bi bi-trash"></i>
         </span>
-    </li>`
-    })
-}
+    </li>`;
+  });
+};
 
-form.onsubmit = (e) => {
-    e.preventDefault();
-    let newToDo = {
-        id: Date.now(),
-        title: e.target[0].value
-    };
-    if (newToDo.title !== "") {
-        if(!isEdit) ToDo.push(newToDo); 
-        else {
-            let newArr = [];
-            ToDo.map(todo => {
-                if (todo.id === whichEl) {
-                    todo.title = newToDo.title;
-                } 
-                newArr.push(todo);
-            })
-            ToDo = newArr;
-            isEdit = false;
-            whichEl = null;
-            input.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add'
+ toDoCardForm.onsubmit = (e) => {
+  e.preventDefault();
+  let newToDo = {
+    id: Date.now(),
+    title: e.target[0].value,
+  };
+  if (newToDo.title !== "") {
+    if (!isEditMode) ToDo.push(newToDo);
+    else {
+      let newToDoList = [];
+      ToDo.forEach((todo) => {
+        if (todo.id === selectedElement) {
+          todo.title = newToDo.title;
         }
-        WriteTodos();
-        form.reset();
-    } else alert("Fill the gap!");
-}
+        newToDoList.push(todo);
+      });
+      ToDo = newToDoList;
+      isEditMode = false;
+      selectedElement = null;
+      toDoCardInput.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add';
+    }
+    createTodo();
+     toDoCardForm.reset();
+  } else alert("Fill the gap!");
 
-form2.onsubmit = (e) => {
-    e.preventDefault();
-    let newToDo = {
-        id: Date.now(),
-        title: e.target[0].value
-    };
-    if (newToDo.title !== "") {
-        if(!isEdit) ndCardList.push(newToDo); 
-        else {
-            let newArr = [];
-            ndCardList.map(todo => {
-                if (todo.id === whichEl) {
-                    todo.title = newToDo.title;
-                } 
-                newArr.push(todo);
-            })
-            ndCardList = newArr;
-            isEdit = false;
-            whichEl = null;
-            input2.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add'
-        }
-        WriteTodos2();
-        form2.reset();
-    } else alert("Fill the gap!");
-}
+  
+  localStorage.setItem("newToDo", JSON.stringify(newToDo))
 
-form3.onsubmit = (e) => {
-    e.preventDefault();
-    let newToDo = {
-        id: Date.now(),
-        title: e.target[0].value
-    };
-    if (newToDo.title !== "") {
-        if(!isEdit) rdCardList.push(newToDo); 
-        else {
-            let newArr = [];
-            rdCardList.map(todo => {
-                if (todo.id === whichEl) {
-                    todo.title = newToDo.title;
-                } 
-                newArr.push(todo);
-            })
-            rdCardList = newArr;
-            isEdit = false;
-            whichEl = null;
-            input3.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add'
+  
+};
+
+doingCardForm .onsubmit = (e) => {
+  e.preventDefault();
+  let newToDo = {
+    id: Date.now(),
+    title: e.target[0].value,
+  };
+  if (newToDo.title !== "") {
+    if (!isEditMode) doingCardList.push(newToDo);
+    else {
+      let newToDoList = [];
+      doingCardList.map((todo) => {
+        if (todo.id === selectedElement) {
+          todo.title = newToDo.title;
         }
-        WriteTodos3();
-        form3.reset();
-    } else alert("Fill the gap!");
-}
+        newToDoList.push(todo);
+      });
+      doingCardList = newToDoList;
+      isEditMode = false;
+      selectedElement = null;
+      doingCardInput.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add';
+    }
+    createDoingList();
+    doingCardForm .reset();
+  } else alert("Fill the gap!");
+
+  localStorage.setItem("newToDo", JSON.stringify(newToDo))
+};
+
+doneCardForm.onsubmit = (e) => {
+  e.preventDefault();
+  let newToDo = {
+    id: Date.now(),
+    title: e.target[0].value,
+  };
+  if (newToDo.title !== "") {
+    if (!isEditMode) doneCardList.push(newToDo);
+    else {
+      let newToDoList = [];
+      doneCardList.map((todo) => {
+        if (todo.id === selectedElement) {
+          todo.title = newToDo.title;
+        }
+        newToDoList.push(todo);
+      });
+      doneCardList = newToDoList;
+      isEditMode = false;
+      selectedElement = null;
+      doneCardInput.nextElementSibling.innerHTML = '<i class="bi bi-plus"></i> Add';
+    }
+    createDoneList();
+    doneCardForm.reset();
+  } else alert("Fill the gap!");
+
+  localStorage.setItem("newToDo", JSON.stringify(newToDo))
+};
 
 const DeleteToDo = (id) => {
-    let confirmation = window.confirm("Are you sure to delete this todo?")
-    if(confirmation) ToDo = ToDo.filter(todo => todo.id !== id)   
-    WriteTodos();
-}
-const DeleteToDo2 = (id) => {
-    let confirmation = window.confirm("Are you sure to delete this todo?")
-    if(confirmation) ndCardList = ndCardList.filter(todo => todo.id !== id)   
-    WriteTodos2();
-}
-const DeleteToDo3 = (id) => {
-    let confirmation = window.confirm("Are you sure to delete this todo?")
-    if(confirmation) rdCardList = rdCardList.filter(todo => todo.id !== id)   
-    WriteTodos3();
-}
+  let confirmation = window.confirm("Are you sure to delete this todo?");
+  if (confirmation) ToDo = ToDo.filter((todo) => todo.id !== id);
+  createTodo();
+};
+const deleteDoingList = (id) => {
+  let confirmation = window.confirm("Are you sure to delete this todo?");
+  if (confirmation) doingCardList = doingCardList.filter((todo) => todo.id !== id);
+  createDoingList();
+};
+const deleteDoneList = (id) => {
+  let confirmation = window.confirm("Are you sure to delete this todo?");
+  if (confirmation) doneCardList = doneCardList.filter((todo) => todo.id !== id);
+  createDoneList();
+};
 
 const EditToDo = (id) => {
-    const input = document.createElement('input');
-    input.type = 'text'
-    isEdit = true;
-    whichEl = id;
-    ToDo.map(todo => {
-        if (todo.id === id) {
-            input.textContent = todo.title;
-        }
-    })
-    
-      
-    // input.nextElementSibling.appendChild ('<i class="bi bi-pencil-square"></i> Edit')
-}
+    isEditMode = true;
+    selectedElement = id;
+    ToDo.forEach((todo) => {
+      if (todo.id === id) {
+        toDoCardInput.value = todo.title;
+      }
+    });
+    toDoCardInput.nextElementSibling.innerHTML =
+      '<i class="bi bi-pencil-square"></i> Edit';
 
-const EditToDo2 = (id) => {
-    isEdit = true;
-    whichEl = id;
-    ndCardList.map(todo => {
-        if (todo.id === id) {
-            input2.value = todo.title;
-        }
-    })
-    input2.nextElementSibling.innerHTML = '<i class="bi bi-pencil-square"></i> Edit'
-}
+  // input.nextElementSibling.appendChild ('<i class="bi bi-pencil-square"></i> Edit')
+};
 
-const EditToDo3 = (id) => {
-    isEdit = true;
-    whichEl = id;
-    rdCardList.map(todo => {
-        if (todo.id === id) {
-            input3.value = todo.title;
-        }
-    })
-    input3.nextElementSibling.innerHTML = '<i class="bi bi-pencil-square"></i> Edit'
-}
+const editDoingList = (id) => {
+  isEditMode = true;
+  selectedElement = id;
+  doingCardList.map((todo) => {
+    if (todo.id === id) {
+        doingCardInput.value = todo.title;
+    }
+  });
+  doingCardInput.nextElementSibling.innerHTML =
+    '<i class="bi bi-pencil-square"></i> Edit';
+};
+
+const editDoneList = (id) => {
+  isEditMode = true;
+  selectedElement = id;
+  doneCardList.map((todo) => {
+    if (todo.id === id) {
+        doneCardInput.value = todo.title;
+    }
+  });
+  doneCardInput.nextElementSibling.innerHTML =
+    '<i class="bi bi-pencil-square"></i> Edit';
+};
 
 function allowDrop(ev) {
-    ev.preventDefault();
-  }
-  
-  function drag(ev) {
-    ev.dataTransfer.setData("text", ev.target.id);
-  }
-  
-  function drop(ev) {
-    ev.preventDefault();
-    var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
-  }
- 
+  ev.preventDefault();
+}
 
-window.onload = () => WriteTodos(), WriteTodos2(), WriteTodos3() 
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+}
+
+(window.onload = () => createTodo()), createDoingList(), createDoneList();
